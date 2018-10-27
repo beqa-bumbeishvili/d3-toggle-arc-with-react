f57c95be5caf62f9d9e7d63ebe93fceebfc6665c
beqa-bumbeishvili <bumbeishvili.beqa@gmail.com>
2018-10-28 00:57:46 +0400

    properties bugfix

--- react-d3-example/src/ProgressArc.js
+++ react-d3-example/src/ProgressArc.js
@@ -1,55 +1,49 @@
 import React, { Component } from 'react';
 import * as d3 from "d3";
 class ProgressArc extends Component {
-    displayName: 'ProgressArc';
-
+    displayName: 'ProgressArc'; 
     propTypes: {
-        id: PropTypes.string,
-        height: PropTypes.number,
-        width: PropTypes.number,
-        innerRadius: PropTypes.number,
-        outerRadius: PropTypes.number,
-        backgroundColor: PropTypes.string,
-        foregroundColor: PropTypes.string,
-        percentComplete: PropTypes.number
+      id: PropTypes.string,
+      height: PropTypes.number,
+      width: PropTypes.number,
+      innerRadius: PropTypes.number,
+      outerRadius: PropTypes.number,
+      backgroundColor: PropTypes.string,
+      foregroundColor: PropTypes.string,
+      percentComplete: PropTypes.number
     }
-
     componentDidMount() {
-        const context = this.setContext();
-        this.setBackground(context);
-        this.setForeground(context)
+      const context = this.setContext();
+      this.setBackground(context);
+      this.setForeground(context);
     }
-
     setContext() {
-        return d3.select(this.refs.arc).append('svg')
-            .attr('height', height)
-            .attr('width', width)
-            .attr('id', id)
-            .append('g')
-            .attr('transform', `translate(${height / 2}, ${width / 2})`);
+      const { height, width, id} = this.props;
+      return d3.select(this.refs.arc).append('svg')
+        .attr('height', height)
+        .attr('width', width)
+        .attr('id', id)
+        .append('g')
+        .attr('transform', `translate(${height / 2}, ${width / 2})`);
     }
-
     setBackground(context) {
-        return context.append('path')
-            .datum({ endAngle: this.tau })
-            .style('fill', backgroundColor)
-            .attr('d', this.arc());
+      return context.append('path')
+      .datum({ endAngle: this.tau })
+      .style('fill', this.props.backgroundColor)
+      .attr('d', this.arc());
     }
-
     setForeground(context) {
-        return context.append('path')
-            .datum({ endAngle: this.tau * percentComplete })
-            .style('fill', foregroundColor)
-            .attr('d', this.arc());
+      return context.append('path')
+      .datum({ endAngle: this.tau * this.props.percentComplete })
+      .style('fill', this.props.foregroundColor)
+      .attr('d', this.arc());
     }
-
     tau = Math.PI * 2;
-
     arc() {
-        return d3.arc()
-            .innerRadius(innerRadius)
-            .outerRadius(outerRadius)
-            .startAngle(0)
+      return d3.arc()
+        .innerRadius(this.props.innerRadius)
+        .outerRadius(this.props.outerRadius)
+        .startAngle(0)
     }
 
     render() {
