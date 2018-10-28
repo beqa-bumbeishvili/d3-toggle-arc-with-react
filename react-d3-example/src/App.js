5d50e361afa54dd677238cc19130e6399aa5f147
beqa-bumbeishvili <bumbeishvili.beqa@gmail.com>
2018-10-28 10:13:15 +0400

    add toggle animation

--- react-d3-example/src/App.js
+++ react-d3-example/src/App.js
@@ -2,17 +2,31 @@ import React, { Component } from 'react';
 import ProgressArc from './ProgressArc';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { percentComplete: 0.3 };
+    this.togglePercent = this.togglePercent.bind(this);
+  }
+  togglePercent() {
+    const percentage = this.state.percentComplete === 0.3 ? 0.7 : 0.3;
+    this.setState({ percentComplete: percentage });
+  }
+
   render() {
-    return <ProgressArc
-      id="d3-arc"
-      height={300}
-      width={300}
-      innerRadius={100}
-      outerRadius={110}
-      backgroundColor="#e6e6e6"
-      foregroundColor="#00ff00"
-      percentComplete={0.3}
-    />;
+    return (
+      <div>
+        <a onClick={this.togglePercent}>Toggle Arc</a>
+        <ProgressArc
+          id="d3-arc"
+          height={300}
+          width={300}
+          innerRadius={100}
+          outerRadius={110}
+          backgroundColor="#e6e6e6"
+          foregroundColor="#00ff00"
+          percentComplete={this.state.percentComplete}
+        /></div>);
   }
 }
 
